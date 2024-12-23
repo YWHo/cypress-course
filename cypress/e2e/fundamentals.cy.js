8df45023e3e17e0f74892c04de7489415e5fad3d
Brian Ho <brian.ho@jucyworld.com>
2024-12-23 13:34:12 +1300

    Write fundamental tests

--- cypress/e2e/fundamentals.cy.js
+++ cypress/e2e/fundamentals.cy.js
@@ -1,5 +1,17 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Fundamentals test', () => {
+  beforeEach(() => {
+    cy.visit('/fundamentals')
+  });
+
+  it('Contains correct header test', () => {
+    cy.get('[data-test="fundamentals-header"]').should('contain.text', 'Testing Fundamentals')
+  })
+
+  it('Accordion works correctly', () => {
+    cy.contains(/Your tests will exist in a describe block/i).should('not.be.visible')
+    cy.get('[data-test="accordion-item-1"] div[role="button"]').click()
+    cy.contains(/Your tests will exist in a describe block/i).should('be.visible')
+    cy.get('[data-test="accordion-item-1"] div[role="button"]').click()
+    cy.contains(/Your tests will exist in a describe block/i).should('not.be.visible')
   })
 })
