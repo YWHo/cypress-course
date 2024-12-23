03a1a97351f426bf2533089fac97cec8360c9207
Brian Ho <brian.ho@jucyworld.com>
2024-12-23 18:00:51 +1300

    Useful methods ('within', 'its', 'first') to test a list

--- cypress/e2e/examples.cy.js
+++ cypress/e2e/examples.cy.js
@@ -24,4 +24,56 @@ describe('Various examples', () => {
         
         cy.getDataTest('post-button').click();
     })
+
+    it.only('grudges', () => {
+        cy.contains(/Add Some Grudges/i);
+
+        cy.getDataTest('grudge-list').within(() => {
+            cy.get('li').should('have.length', 0);
+        })
+
+        cy.getDataTest('clear-button').should('not.exist');
+
+        cy.getDataTest('grudge-list-title').should('have.text', 'Add Some Grudges');
+
+        cy.getDataTest('grudge-input').within(() => {
+            cy.get('input').type('grudge 1');
+        })
+        cy.getDataTest('add-grudge-button').click();
+
+        cy.getDataTest('grudge-list').within(() => {
+            cy.get('li').should('have.length', 1);
+        })
+
+        cy.getDataTest('grudge-list-title').should('have.text', 'Grudges');
+
+        cy.getDataTest('grudge-input').within(() => {
+            cy.get('input').type('grudge 2');
+        })
+        cy.getDataTest('add-grudge-button').click();
+        
+        cy.getDataTest('grudge-list').within(() => {
+            cy.get('li').should('have.length', 2);
+            cy.get('li').its(0).should('contains.text', 'grudge 1');
+            cy.get('li').first().should('contains.text', 'grudge 1');
+        })
+
+        cy.getDataTest('grudge-list').within(() => {
+            cy.get('li').its(0).within(() => {
+                cy.get('button').click();
+            })
+        })
+
+        cy.getDataTest('grudge-list').within(() => {
+            cy.get('li').should('have.length', 1);
+        })
+
+        cy.getDataTest('clear-button').click();
+
+        cy.getDataTest('grudge-list').within(() => {
+            cy.get('li').should('have.length', 0);
+        })
+
+        cy.getDataTest('clear-button').should('not.exist');
+    })
 })
